9aebf8093769486b77867982597545356ea68971
Ho Doan Ba Thien <bathien2601@gmail.com>
2019-05-11 09:01:17 +0700

    init rails and genenrate basic model

--- db/migrate/20190510001213_create_games.rb
+++ db/migrate/20190510001213_create_games.rb
@@ -2,10 +2,10 @@ class CreateGames < ActiveRecord::Migration[5.2]
   def change
     create_table :games do |t|
       t.references :match, foreign_key: true
-      t.references :player_1, foreign_key: { to_table: :players }, index: true
-      t.references :player_2, foreign_key: { to_table: :players }, index: true
-      t.integer :player_1_score, default: 0
-      t.integer :player_2_score, default: 0
+      t.references :team_1, foreign_key: { to_table: :players }, index: true
+      t.references :team_2, foreign_key: { to_table: :players }, index: true
+      t.integer :team_1_score, default: 0
+      t.integer :team_2_score, default: 0
       t.integer :status
       t.timestamps
     end
